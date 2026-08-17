10466bd17aa6cc684be4df5e4e45d020f4f77443
Mohana Prasath R <mohanaprasath1526@gmail.com>
2026-08-17 17:02:00 +0530

    style: replace complex logo paths with crystal-clear solid white block M and P vector shapes for 100% legibility

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -112,30 +112,20 @@ export default function Navbar() {
             {/* Ambient inner red glow */}
             <div className="absolute inset-0 bg-gradient-to-tr from-red-600/30 via-transparent to-amber-500/20 opacity-70 group-hover:opacity-100 transition-opacity duration-500" />
 
-            {/* Pure White Sharp 3D Monogram Vector SVG */}
+            {/* Crystal Clear Solid White Bold MP Monogram */}
             <svg
-              viewBox="0 0 260 200"
+              viewBox="0 0 230 140"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
               className="w-7 h-7 relative z-10 transition-transform duration-500 group-hover:scale-110"
             >
               <path
-                d="M 28 55 L 50 32 H 78 L 62 55 V 140 L 42 162 H 28 V 55 Z
-                   M 62 55 L 122 135 L 182 55 V 140 L 162 162 H 146 V 55 Z
-                   M 162 55 L 180 32 H 222 C 244 32 254 46 254 68 C 254 90 244 104 222 104 H 192 V 148 L 172 168 H 156 V 55 Z
-                   M 192 52 H 218 C 230 52 236 58 236 68 C 236 78 230 84 218 84 H 192 V 52 Z"
+                d="M 16 120 V 20 H 34 L 70 82 L 106 20 H 124 V 120 H 104 V 58 L 75 108 H 65 L 36 58 V 120 H 16 Z"
                 fill="#FFFFFF"
-                stroke="#F8FAFC"
-                strokeWidth="3"
-                strokeLinejoin="miter"
               />
               <path
-                d="M 36 58 L 48 44 H 68 L 56 58 V 132 L 44 148 H 36 Z
-                   M 56 58 L 122 126 L 174 58 V 132 L 164 148 H 154 V 58 Z
-                   M 170 58 L 182 44 H 214 C 230 44 238 52 238 68 C 238 84 230 92 214 92 H 184 V 138 L 170 152 Z
-                   M 184 60 H 210 C 220 60 224 63 224 68 C 224 73 220 76 210 76 H 184 Z"
+                d="M 140 120 V 20 H 186 C 206 20 216 32 216 54 C 216 76 206 88 186 88 H 162 V 120 H 140 Z M 162 40 V 68 H 184 C 192 68 196 63 196 54 C 196 45 192 40 184 40 H 162 Z"
                 fill="#FFFFFF"
-                opacity="0.9"
               />
             </svg>
           </div>
